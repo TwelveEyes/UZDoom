4b783386cd46d789a9c8a12e9a5ee48ed21fee8e
Xaser Acheron <xaser.88@gmail.com>
2025-11-08 00:57:52 -0600

    add "blockrendering" sidedef flag; instructs the renderer to clip the world beyond (like a 1-sided line does), but only for that side.
    # Conflicts:
    #	src/rendering/hwrenderer/scene/hw_bsp.cpp

--- src/maploader/udmf.cpp
+++ src/maploader/udmf.cpp
@@ -1420,6 +1420,10 @@ public:
 				Flag(sd->Flags, WALLF_NOAUTODECALS, key);
 				continue;
 
+			case NAME_Blockrendering:
+				Flag(sd->Flags, WALLF_BLOCKRENDERING, key);
+				continue;
+
 			case NAME_colorization_top:
 				sd->SetTextureFx(side_t::top, TexMan.GetTextureManipulation(CheckString(key)));
 				break;
