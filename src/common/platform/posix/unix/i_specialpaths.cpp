9436c9b299149a9c236dc8775996b558be3c5e7d
Marcus Minhorst <marcus.minhorst@gmail.com>
2025-11-22 17:05:22 -0500

    Update Linux/Haiku paths
    Co-Authored-By: Peppersawce <michaelpeppers89@yahoo.it>

--- src/common/platform/posix/unix/i_specialpaths.cpp
+++ src/common/platform/posix/unix/i_specialpaths.cpp
@@ -73,6 +73,7 @@ DEFGETPATH(Data, "XDG_DATA_HOME", "$HOME/config/non-packaged/data");
 DEFGETPATH(Config, "XDG_CONFIG_HOME", "$HOME/.config");
 DEFGETPATH(Cache, "XDG_CACHE_HOME", "$HOME/.cache");
 DEFGETPATH(Data, "XDG_DATA_HOME", "$HOME/.local/share");
+DEFGETPATH(Pictures, "XDG_PICTURES_DIR", "$HOME/Pictures");
 #endif
 #undef DEFGETPATH
 
@@ -195,7 +196,11 @@ FString M_GetConfigPath(bool for_reading)
 
 FString M_GetDocumentsPath()
 {
+#ifdef __HAIKU__
+	return FStringf("%s/" GAMENAMELOWERCASE "/", GetConfigPath());
+#else
 	return M_GetAppDataPath(false) + "/";
+#endif
 }
 
 
@@ -209,7 +214,13 @@ FString M_GetDocumentsPath()
 
 FString M_GetScreenshotsPath()
 {
-	return M_GetDocumentsPath() + "screenshots/";
+#ifdef __HAIKU__
+	static FString path = M_GetDocumentsPath() + "screenshots";
+#else
+	static FString path = FStringf("%s/Screenshots/" GAMENAME, GetPicturesPath());
+#endif
+	path = NicePath(path.GetChars());
+	return path;
 }
 
 //===========================================================================
