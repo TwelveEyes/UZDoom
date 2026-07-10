7fd44a9676d030d84f4bbdacebe4cdfc52df1a42
drfrag <drfrag666@hotmail.com>
2026-07-10 20:50:55 +0200

    - Bump version number.

--- src/version.h
+++ src/version.h
@@ -41,21 +41,21 @@ const char *GetVersionString();
 
 /** Lots of different version numbers **/
 
-#define VERSIONSTR "4.14.3"
+#define VERSIONSTR "4.14.4"
 
 // The version as seen in the Windows resource
-#define RC_FILEVERSION 4,14,3,0
-#define RC_PRODUCTVERSION 4,14,3,0
+#define RC_FILEVERSION 4,14,4,0
+#define RC_PRODUCTVERSION 4,14,4,0
 #define RC_PRODUCTVERSION2 VERSIONSTR
 // These are for content versioning.
 #define VER_MAJOR 4
 #define VER_MINOR 14
-#define VER_REVISION 3
+#define VER_REVISION 4
 
 // This should always refer to the GZDoom version a derived port is based on and not reflect the derived port's version number!
 #define ENG_MAJOR 4
 #define ENG_MINOR 14
-#define ENG_REVISION 3
+#define ENG_REVISION 4
 
 // Version identifier for network games.
 // Bump it every time you do a release unless you're certain you
