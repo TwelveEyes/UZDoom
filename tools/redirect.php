6272733d24a1b60ebbe6d4c134314c3f45c75b4d
Marcus Minhorst <marcus.minhorst@gmail.com>
2026-07-20 22:18:36 -0400

    Add redirect script

---
+++ tools/redirect.php
@@ -0,0 +1,17 @@
+<?php
+
+$repo = "UZDoom/UZDoom";
+
+$release = $_GET["r"];
+if (empty($release) || str_contains($release, '/')) die();
+
+$asset = $_GET["f"];
+if (empty($asset) || str_contains($asset, '/')) die();
+
+$url = "https://github.com/".$repo."/releases/download/".$release."/".$asset;
+
+header("Location: " . $url, true, 302);
+
+exit();
+
+?>
